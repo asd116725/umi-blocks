76ee7481d05c15adb1b592671e9850106f7c9cfd
WuJiXiong <361760235@qq.com>
2019-11-13 17:30:54 +0800

    2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import styles from './index.css';
 export default function() {
   return (
     <div className={styles.normal}>
-      <h1>I am a umi block!</h1>
+      <h1>I am a umi block!s</h1>
     </div>
   );
 }
